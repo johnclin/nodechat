53c25ca28b3706542dc71ce726c6ea6ec1f1f64e
johnclin <faerellg@gmail.com>
2015-10-06 16:34:58 -0700

    object oriented

--- fayeclient.js
+++ fayeclient.js
@@ -2,5 +2,5 @@ var faye = require('faye');
 var inputParser = require('./inputParser');
 var client = new faye.Client('http://localhost:8000/faye');
 console.log('Welcome to Chat');
-inputParser.initParser(client);
-inputParser.listen();
+inputParser.chatApp.initParser(client);
+inputParser.chatApp.listen();
--- inputParser.js
+++ inputParser.js
@@ -1,53 +1,72 @@
 /**
  * Created by John Lin on 10/6/2015.
  */
+var inputParser = inputParser || {};
 
-var inputParser = {
-    statesEnum: null,
+inputParser.statesEnum = {
+    USERNAME: 0,
+    CHANNEL: 1,
+    INCHAT: 2
+};
+
+inputParser.userInfo = {
+    username: null,
+    channel: null,
+    state: null
+};
+
+inputParser.connectionInfo = {
+    stdin: null,
+    client: null
+};
+
+inputParser.chatApp = {
     username: null,
     channel: null,
     state: null,
     client: null,
 
     initParser: function(client){
-        var statesEnum = {
-            USERNAME: 0,
-            CHANNEL: 1,
-            INCHAT: 2
-        };
-
-        this.statesEnum = statesEnum;
-        this.util = require("util");
-        this.stdin = process.openStdin();
-        this.username = this.statesEnum.USERNAME;
-        this.client = client;
-        
-        console.log('Please enter your name:');
+        var util = require("util");
+        inputParser.connectionInfo.stdin = process.openStdin();
+        inputParser.userInfo.state = inputParser.statesEnum.USERNAME;
+        inputParser.connectionInfo.client = client;
+        switch(inputParser.userInfo.state){
+            case inputParser.statesEnum.USERNAME:
+                console.log('Please enter your name:');
+                break;
+            case inputParser.statesEnum.CHANNEL:
+                console.log('Please select Channel:');
+                break;
+        }
     },
 
     listen: function(){
-        this.stdin.addListener("data", function(msg) {
+        inputParser.connectionInfo.stdin.addListener("data", function(msg) {
             var msgString = msg.toString().trim();
-
-            switch(this.state){
-                case this.statesEnum.USERNAME:
-                    this.username = msgString;
+            switch(inputParser.userInfo.state){
+                case inputParser.statesEnum.USERNAME:
+                    inputParser.userInfo.username = msgString;
                     console.log('Now chatting as ' + msgString);
+                    inputParser.userInfo.state = inputParser.statesEnum.CHANNEL;
                     break;
-                case this.statesEnum.CHANNEL:
-                    if(this.channel != null){
-                        client.unsubscribe(this.channel);
+                case inputParser.statesEnum.CHANNEL:
+                    if(inputParser.userInfo.channel != null){
+                        console.log('Leaving ' + inputParser.userInfo.channel);
+                        inputParser.connectionInfoclient.unsubscribe(inputParser.userInfo.channel);
                     }
-                    this.channel = '/' + msgString;
-                    this.client.subscribe(this.channel, function(message){
-                        this.username;
+                    inputParser.userInfo.channel = '/' + msgString;
+                    console.log('Entering ' + inputParser.userInfo.channel);
+                    inputParser.connectionInfo.client.subscribe(inputParser.userInfo.channel, function(message){
+                        inputParser.userInfo.username;
                         console.log(message);
                     });
+                    inputParser.userInfo.state = inputParser.statesEnum.INCHAT;
                     break;
-                case this.statesEnum.INCHAT:
+                case inputParser.statesEnum.INCHAT:
                     //scan for commands
                     //if no commands chat
-                    var publication = this.client.publish(this.channel, msg.toString().trim());
+                    var publication = inputParser.connectionInfo.client.publish(inputParser.userInfo.channel, msg.toString().trim());
                     publication.then(function() {
                         console.log('Message received by server!');
                     }, function(error) {
