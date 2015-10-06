67b8d12241aeaf70998e99d311a185eb16ce6cb6
johnclin <faerellg@gmail.com>
2015-10-06 14:42:23 -0700

    object oriented

--- fayeclient.js
+++ fayeclient.js
@@ -3,5 +3,5 @@ var inputParser = require('./inputParser');
 var client = new faye.Client('http://localhost:8000/faye');
 console.log('Welcome to Chat');
 
-inputParser.initParser();
+inputParser.initParser(client);
 inputParser.listen();
--- inputParser.js
+++ inputParser.js
@@ -10,56 +10,46 @@ var inputParser = {
     },
     username: null,
     channel: null,
-    state: states.username,
+    state: this.states.username,
+    client: null,
 
-    initParser: function(){
+    initParser: function(client){
         this.util = require("util");
         this.stdin = process.openStdin();
 
-        this.
+        this.client = client;
 
         console.log('Please enter your name:');
     },
 
     listen: function(){
-        stdin.addListener("data", function(msg) {
+        this.stdin.addListener("data", function(msg) {
             var msgString = msg.toString().trim();
             switch(state){
-                case states.username:
-                    username = msgString;
+                case this.states.username:
+                    this.username = msgString;
                     console.log('Now chatting as ' + msgString);
                     break;
-                case states.channel:
+                case this.states.channel:
                     if(this.channel != null){
                         client.unsubscribe(this.channel);
                     }
                     this.channel = '/' + msgString;
-                    client.subscribe(this.channel, function(message){
+                    this.client.subscribe(this.channel, function(message){
                         this.username;
                         console.log(message);
                     });
                     break;
-                case states.inchat:
+                case this.states.inchat:
                     //scan for commands
                     //if no commands chat
-                    var publication = client.publish('/message', msg.toString().trim());
+                    var publication = this.client.publish(this.channel, msg.toString().trim());
                     publication.then(function() {
                         console.log('Message received by server!');
                     }, function(error) {
                         console.log('There was a problem: ' + error.message);
                     });
                     break;
-            }
-            if (username == null){
-                username = msg;
-                console.log('Now chatting as ' + msg);
-            }else{
-                var publication = client.publish('/message', msg.toString().trim());
-                publication.then(function() {
-                    console.log('Message received by server!');
-                }, function(error) {
-                    console.log('There was a problem: ' + error.message);
-                });
             }
         });
     }
